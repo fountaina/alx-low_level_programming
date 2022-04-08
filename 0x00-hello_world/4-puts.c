d5450849e04aae249f3091f837105220b4709d5a
Fountain <fountainaigbona@gmail.com>
2022-04-08 01:37:58 +0100

    fixed betty and output issues for 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints standard output
- * Description: This simply ouputs a string message 
+ * Description: This simply ouputs a string message
  * Return: passes all test if return value is 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
