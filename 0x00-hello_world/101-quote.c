708455d868c979a8ffca9e347456a5ca49ca5454
Fountain <fountainaigbona@gmail.com>
2022-04-08 03:26:03 +0100

    created 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a message
+ * Description - prints a message to the user
+ * Return: should return 1 on sucess
+ */
+int main(void)
+{
+	fprintf(stdout, "and the piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
