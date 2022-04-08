6f284fae6b1bdf1f02d2df9a9180a6a815fd80f6
Fountain <fountainaigbona@gmail.com>
2022-04-08 01:43:33 +0100

    created the 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints standard output
+ * Description: prints a string message to user
+ * Return: a successful run returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+}
