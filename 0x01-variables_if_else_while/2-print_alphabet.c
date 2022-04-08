766dce2d560ff41da68ca7201bef084187fd0049
Fountain <fountainaigbona@gmail.com>
2022-04-08 23:41:53 +0100

    fixed the very annoying betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,12 @@
 /**
  * main - print all alphabet
  * Description: this prints all alphabet to stdout using
- * putchar from a predefined string
  * Return: returns 0 on sucessful output
  */
 int main(void)
 {
 	char s;
-	
+
 	for (s = 'a'; s <= 'z'; s++)
 		putchar(s);
 	putchar('\n');
