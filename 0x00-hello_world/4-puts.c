0b553a0d248cda0310e0e0cff8114b070ba894c2
Fountain <fountainaigbona@gmail.com>
2022-04-08 01:34:25 +0100

    betty hot fix for 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main ()
+/**
+ * main - prints standard output
+ * Description: This simply ouputs a string message 
+ * Return: passes all test if return value is 0
+ */
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
