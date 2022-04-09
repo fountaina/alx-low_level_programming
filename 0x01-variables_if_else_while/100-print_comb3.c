88e09249623316b6e0aa2221b651040f1311952d
Fountain <fountainaigbona@gmail.com>
2022-04-09 04:13:17 +0100

    testing my 100-print_combo3.c again

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - prints two digits
- * Description: prints all possible differrent combinations of 
+ * Description: prints all possible differrent combinations of
  * two digits.
  * Return: returns 0 for success
  */
@@ -11,14 +11,17 @@ int main(void)
 	int num2;
 	int flag;
 
-	for (num1 = '0'; num1 <= '9'; num1++)
+	for (num1 = '0'; num1 <= '8'; num1++)
 	{
 		putchar(num1);
 		flag = 0;
 		for (num2 = '0'; num2 <= '9'; num2++)
 		{
 			putchar(num2);
-			if (num1 + num2 != 18)
+			if (num1 == '8' &&  num2 == '9')
+			{
+				continue;
+			} else
 			{
 				putchar(',');
 				putchar(' ');
