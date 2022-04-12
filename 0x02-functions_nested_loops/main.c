083274a13a39361ea00af3e101f1eb3ae63879b3
Fountain <fountainaigbona@gmail.com>
2022-04-12 14:01:25 -0700

    testing

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+include "main.h"
+include "1-alphabet.c"
+
+/**
+ *  * main - check the code for Holberton School students.
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+		print_alphabet();
+		return (0);
+}
