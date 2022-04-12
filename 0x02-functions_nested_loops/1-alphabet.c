ca40527d78d0251302e85c5e0badae3061ac36c1
Fountain <fountainaigbona@gmail.com>
2022-04-12 20:31:06 +0100

    fixed the betty bugs in 1-alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,7 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - prints the alphabets
- * 
  * print_alphabet - prints out the alphabets
- * Description -  prints out the alphabts
  * Return: 0 on sucessful output
  */
 void print_alphabet(void)
@@ -14,12 +10,7 @@ void print_alphabet(void)
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		_putchar(alpha);	
+		_putchar(alpha);
 	}
 	putchar('\n');
-}
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
