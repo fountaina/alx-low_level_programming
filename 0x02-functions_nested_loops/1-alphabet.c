5e79ba1522f693dd4bc2de319314728b018bc53d
Fountain <fountainaigbona@gmail.com>
2022-04-12 20:43:29 +0100

    testing the 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,4 @@
  * print_alphabet - prints out the alphabets
  * Return: 0 on sucessful output
  */
-void print_alphabet(void)
-{
-	char alpha;
-
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	{
-		_putchar(alpha);
-	}
-	_putchar('\n');
-}
+void print_alphabet(void);
