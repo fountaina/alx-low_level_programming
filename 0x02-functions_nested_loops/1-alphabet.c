67f15b9d905e2bb48c75996abdd296d8ee9eba0a
Fountain <fountainaigbona@gmail.com>
2022-04-12 20:19:15 +0100

    edited stuffs in the alphabets code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,23 @@
 
 /**
  * main - prints the alphabets
- *
+ * 
+ * print_alphabet - prints out the alphabets
+ * Description -  prints out the alphabts
  * Return: 0 on sucessful output
  */
-int main(void)
+void print_alphabet(void)
 {
 	char alpha;
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		_putchar(alpha);
+		_putchar(alpha);	
 	}
+	putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
 	return (0);
 }
