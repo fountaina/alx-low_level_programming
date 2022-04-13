3bdd0902929e3bfdd98b36b16a598a1bcc6fdb66
Fountain <fountainaigbona@gmail.com>
2022-04-13 03:33:18 +0100

    wrote the 5-sign.c code

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number inputed by the user
+ *
+ * Return: returns 1 and prints + if n > 0
+ * returns 0 and prints 0 if n is 0
+ * returns -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	} else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	} else
+	{
+		value = -1;
+		_putchar('-');
+	}
+	return (return_value);
+}
+
