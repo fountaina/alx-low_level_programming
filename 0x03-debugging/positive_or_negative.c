e2c07e2f64bb593e9d0c194a4d42a6353295518d
Fountain <fountainaigbona@gmail.com>
2022-04-18 14:35:36 +0100

    fixed function conflicts

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints a random number
+ * Description - What the hell. Aah!
+ * Return: returns 0 on sucessful run
+ */
+int positive_or_negative(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	if (n == 0)
+		printf("%i is zero\n", n);
+	if (n < 0)
+		printf("%i is negative\n", n);
+	return (0);
+}
