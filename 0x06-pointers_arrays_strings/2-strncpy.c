f3edd0f7233ab39c9ae504f3c7891b3e23f2a2d8
Fountain <fountainaigbona@gmail.com>
2022-04-25 15:14:56 +0100

    wrote a program thatcopies a string into another string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * _strncpy - copies and paste a string into another
+ * @dest: the home string
+ * @src: the string to be copied
+ * @n: the amount of s2 that is copied
+ *
+ * Return: returns dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+
+	while (b <= n - 1)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (dest);
+}
