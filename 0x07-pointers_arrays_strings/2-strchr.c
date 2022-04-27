e25696255881c5a740e7a3f7d3555fdb8b4790d1
Fountain <fountainaigbona@gmail.com>
2022-04-27 06:00:16 +0100

    wrote a program that behaves just like strchr in C

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string to be searched
+ * @c: the character to find
+ *
+ * Return: the first occurence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	int counter;
+	static char finder_str[98];
+	int finder_str_counter;
+	int found_occurance;
+	int i;
+	
+	counter = 0;
+	finder_str_counter = 0;
+	found_occurance = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (counter <= i)
+	{
+		if (s[counter] == c)
+		{
+			found_occurance = 1;
+			while(s[counter] != '\0')
+			{
+				finder_str[finder_str_counter] = s[counter];
+				counter++;
+				finder_str_counter++;
+			}
+			break;
+		}
+		else
+		{
+			counter++;
+			continue;
+		}
+	}
+	if (found_occurance == 1)
+	{
+		return (finder_str);
+	}
+	else if (found_occurance == 0)
+	{
+		return ('\0');
+	}
+	return (finder_str);
+}
