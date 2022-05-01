582441f41c43b54af0e1052256de5af88c0cc846
Fountain <fountainaigbona@gmail.com>
2022-05-01 22:31:23 +0100

    wrote a program that reverses a string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,38 @@
 #include "main.h"
-
+/**
+ * string_looper - loops through the string and returns it in reverse order
+ * @s: the string
+ * @l: the int that stores the length of the string
+ *
+ * Return: a reversed string
+ */
+char string_looper(char *s, int l)
+{
+	if (s[l] == '\0')
+	{
+		return (l);
+	}
+	else
+	{
+		return (string_looper(s, l + 1));
+	}
+}
+void string_reverser(char *s, int l)
+{
+	if (s[l] == '\0')
+	{
+		l--;
+	}
+	_putchar(s[l]);
+	if (s[l] != 0)
+	{
+		string_reverser(s, l - 1);
+	}
+	else if (s[l] == 0)
+	{
+		_putchar('\0');
+	}
+}
 /**
  * _print_rev_recursion - prints a string in reverse order
  * @s: the string to reverse
@@ -8,19 +41,8 @@
  */
 void _print_rev_recursion(char *s)
 {
-	char **p;
+	int l;
 
-	p = &s;
-	p = (char *)(&s +1) - 1;
-	if (s[0] == 0)
-	{
-		_putchar('\0');
-	}
-	else if (s[0] != '\0')
-	{
-		_putchar(p[0]);
-		--s;
-		_print_rev_recursion(s);
-	}
-
+	l = string_looper(s, 0);
+	string_reverser(s, l);
 }
