e6f4b8e9fb72234bb7ed5de40ab9061d7fd4893d
Fountain <fountainaigbona@gmail.com>
2022-05-01 22:47:00 +0100

    fixed the extra character output; ;

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * string_looper - loops through the string and returns it in reverse order
  * @s: the string
  * @l: the int that stores the length of the string
  *
- * Return: a reversed string
+ * Return: a the length of the string
  */
 char string_looper(char *s, int l)
 {
@@ -18,6 +18,14 @@ char string_looper(char *s, int l)
 		return (string_looper(s, l + 1));
 	}
 }
+
+/**
+ * string_reverser - reverses a string
+ * @s: the string to be reversed
+ * @l: the length of the string
+ *
+ * Return: a reversed string
+ */
 void string_reverser(char *s, int l)
 {
 	if (s[l] == '\0')
@@ -29,10 +37,6 @@ void string_reverser(char *s, int l)
 	{
 		string_reverser(s, l - 1);
 	}
-	/*else if (s[l] == 0)
-	{
-		_putchar('\0');
-	}*/
 }
 /**
  * _print_rev_recursion - prints a string in reverse order
@@ -44,6 +48,6 @@ void _print_rev_recursion(char *s)
 {
 	int l;
 
-	l = string_looper(s, 0);
+	l = string_looper(s, 0) - 1;
 	string_reverser(s, l);
 }
