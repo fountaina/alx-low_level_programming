1e66b6f021f011eb0ffc2820993d6825f58d17a0
Fountain <fountainaigbona@gmail.com>
2022-05-02 22:48:55 +0100

    hot fix

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - returns 1 if c is a letter, lowercase
+ * or uppercase
+ *@c: the input to test
+ *
+ * Return: returns the above
+ */
+int _isalpha(int c)
+{
+	return (c >= 65 && c <= 122);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - print the absolute value of a number
+ * @n: the input integer
+ *
+ * Return: 0 on sucess
+ */
+int _abs(int n)
+{
+	return (n >= 0 ? n : -n);
+}
