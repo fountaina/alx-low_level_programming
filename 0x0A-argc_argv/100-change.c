8f5a7c8b6e057e852c7d465f2edb21319f544d16
Fountain <fountainaigbona@gmail.com>
2022-05-03 05:30:22 +0100

    started advanced task

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins to make change for
+ * an amount of money
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0 on succes
+ */
+int main(int argc, char **argv)
+{
+
