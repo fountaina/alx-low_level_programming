6e5769ce8d198021128e15f2814e0ce18282244a
Fountain <fountainaigbona@gmail.com>
2022-05-03 04:10:59 +0100

    wrote program that multiplies command line arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers from command line input
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int result;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
