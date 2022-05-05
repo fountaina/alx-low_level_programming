674998fa539d98c04af1a3c46b24bc5cd9606b19
Fountain <fountainaigbona@gmail.com>
2022-05-05 23:43:54 +0100

    working on task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @str: the string
+ *
+ * Return: the length
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - returns a copy of a sting
+ * @str: the string to be copied
+ *
+ * Return: returns the string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a pararmeter.
+ * @str: the given string
+ *
+ * Return: returns the pointer
+ */
+char *_strdup(char *str)
+{
+	char *dest;
+
+	dest = (char *) malloc(_strlen(str) + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		_strcpy(dest, str);
+	}
+	return dest;
+}
