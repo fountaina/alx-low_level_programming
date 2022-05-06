5800017507dd0047d580a130b51657c4d0ef4c0c
Fountain <fountainaigbona@gmail.com>
2022-05-06 18:42:02 +0100

    fixed issues with the grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *) malloc(height * sizeof(int));
 		if (arr[i] == NULL)
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,10 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * free_grid - free 2D grid previously created
- * @grid: the grid 
+ * @grid: the grid
  * @height: the height of the array
  *
  * Return: void
  */
 void free_grid(int **grid, int height)
 {
-	int **arr = alloc_grid.c;
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+		grid[i] = NULL;
+	}
+	free(grid);
+	grid = NULL;
+}
