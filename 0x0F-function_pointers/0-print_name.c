04ba4cc997f6c447b6f16430c05aaac3941af88e
Fountain <fountainaigbona@gmail.com>
2022-05-13 23:32:43 +0100

    added header file line to task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include "function_pointers.h"
 
 /**
  * print_name - prints a name.
@@ -9,7 +10,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name[0] == '\0')
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
