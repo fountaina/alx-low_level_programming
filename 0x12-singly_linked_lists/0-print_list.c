c87d27ff061dcd962dfcbf0f4ede9b37f6d1ab8a
Fountain <fountainaigbona@gmail.com>
2022-05-26 18:04:39 +0100

    hot fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,36 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-#include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * _puts - writes a string to stdout.
- * @str: the string to be writtten to stdout.
- *
- * Retrun: nothing
- */
-void  _puts(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-}
 /**
  * print_list - prints the number of elements in a list and their values
  * @h: the first element of the linked list
@@ -40,24 +11,16 @@ void  _puts(char *str)
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
-	unsigned int i;
 
 	while (h)
 	{
 		if (h->str == NULL)
 		{
-			_puts("[0] (nil)");
-			_putchar('\n');
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-			_putchar('[');
-			_putchar((h->len) + '0');
-			_putchar(']');
-			_putchar(' ');
-			for (i = 0; i < (h->len); i++)
-				_putchar(h->str[i]);
-			_putchar('\n');
+			printf("[%u] %s\n", (h->len), (h->str));
 			n++;
 		}
 		h = h->next;
