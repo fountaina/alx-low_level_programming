0016d673469f91a2359eaae8f62ef62172bdf0f9
Fountain <fountainaigbona@gmail.com>
2022-05-27 08:43:02 +0100

    done with task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a linked list.
+ * @h: the linked list
+ *
+ * Return: returns an int of type size_t
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int n = 0;
 
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
