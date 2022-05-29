d85cf0c073d1c4dfe6cf10732eff904744d7a83d
Fountain <fountainaigbona@gmail.com>
2022-05-29 16:38:35 +0100

    added block to free memory if strdup fails

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -31,6 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
+		free(new_node->str);
 		return (NULL);
 	new_node->len = i;
 	new_node->next = NULL;
