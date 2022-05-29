e9f7ad307a7235b4d5ce6f683799df05d27e323d
Fountain <fountainaigbona@gmail.com>
2022-05-29 16:25:09 +0100

    done with task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -19,6 +19,10 @@ list_t *add_node_end(list_t **head, const char *str)
 		;
 	new_node = malloc(sizeof(list_t));
 	last_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (last_node == NULL)
+		return (NULL);
 	new_node->str = strdup(str);
 	new_node->len = i;
 	new_node->next = NULL;
