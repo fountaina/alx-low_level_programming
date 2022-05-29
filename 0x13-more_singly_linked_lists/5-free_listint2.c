dc12c1d487341a812479e2870c5bd9c42803c8e4
Fountain <fountainaigbona@gmail.com>
2022-05-29 19:42:55 +0100

    fixed all issues with task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,13 +12,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *current;
 
 	if (head != NULL)
 	{
-		temp = *head;
-		while (temp != NULL)
+		current = *head;
+		while ((temp = current) != NULL)
 		{
-			temp = temp->next;
+			current = current->next;
 			free(temp);
 		}
 		*head = NULL;
