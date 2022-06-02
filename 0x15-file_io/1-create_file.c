76ceb3d7a88f657ffc74e59c64279b07ca9477fe
Fountain <fountainaigbona@gmail.com>
2022-06-02 05:04:49 +0100

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	op = open(filename, O_CREAT | O_RDWR, 0600);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, _strlen(text_content));
 	if (op == -1 || wr == -1)
 		return (-1);
