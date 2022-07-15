ff8625bd5761d2255bfc0584dfebaa9b86760435
Fountain <fountainaigbona@gmail.com>
2022-07-15 21:14:28 +0100

    created a dynamic library for arithmetic operations

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - adds two integers
+ * @a: the first num
+ * @b: the seconde num
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the difference of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two integers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the quotient of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - gets the modulus of two integers
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the modulus of a and b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
