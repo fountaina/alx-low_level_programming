2d6039bcd82a5fff7d7de362a92151f0b35cd943
Fountain <fountainaigbona@gmail.com>
2022-07-15 18:15:54 +0100

    first move for dynamic libraries

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a given character is lowercase or not
+ *@c: integer to be checked
+ *
+ * Return: returns 1 if lowercase 0 if not
+ */
+int _islower(int c)
+{
+	return (c >=  97 && c <= 122);
+}
